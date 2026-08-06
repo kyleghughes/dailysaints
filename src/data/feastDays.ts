0df60f0cd0631bab189ccbbcde3f08807361796b
kyleghughes <kyle.hughes123@hotmail.co.uk>
2026-08-06 21:58:24 +0100

    Progress

--- src/data/feastDays.ts
+++ src/data/feastDays.ts
@@ -4,8 +4,7 @@ export interface FeastDay {
   name: string;
 }
 
-export const feastDays: FeastDay[] = [
-  /* JANUARY */
+const nonMovingFeastDays: FeastDay[] = [
   {
     month: 1,
     day: 1,
@@ -18,29 +17,13 @@ export const feastDays: FeastDay[] = [
   },
   {
     month: 1,
-    day: 10,
-    name: "Feast of the Baptism of the Lord",
-  },
-  {
-    month: 1,
     day: 25,
     name: "Feast of the Conversion of St. Paul the Apostle",
   },
-  /* FEBRUARY */
   {
     month: 2,
     day: 2,
     name: "Feast of the Presentation of the Lord",
-  },
-  {
-    month: 2,
-    day: 9,
-    name: "Shrove Tuesday",
-  },
-  {
-    month: 2,
-    day: 10,
-    name: "Ash Wednesday",
   },
   {
     month: 2,
@@ -52,7 +35,6 @@ export const feastDays: FeastDay[] = [
     day: 22,
     name: "Feast of the Chair of St. Peter the Apostle",
   },
-  /* MARCH */
   {
     month: 3,
     day: 19,
@@ -60,45 +42,8 @@ export const feastDays: FeastDay[] = [
   },
   {
     month: 3,
-    day: 21,
-    name: "Palm Sunday of the Passion of the Lord",
-  },
-  {
-    month: 3,
     day: 25,
-    name: "Holy Thursday of the Lord's Supper",
-  },
-  {
-    month: 3,
-    day: 26,
-    name: "Good Friday",
-  },
-  {
-    month: 3,
-    day: 27,
-    name: "Holy Saturday",
-  },
-  {
-    month: 3,
-    day: 28,
-    name: "Easter Sunday",
-  },
-  /* APRIL */
-  {
-    month: 4,
-    day: 5,
-    name: "Feast of Divine Mercy",
-  },
-  {
-    month: 4,
-    day: 5,
     name: "Solemnity of the Annunciation of the Lord",
-  },
-  /* MAY */
-  {
-    month: 5,
-    day: 6,
-    name: "Solemnity of the Ascension of the Lord",
   },
   {
     month: 5,
@@ -107,44 +52,8 @@ export const feastDays: FeastDay[] = [
   },
   {
     month: 5,
-    day: 16,
-    name: "Pentecost",
-  },
-  {
-    month: 5,
-    day: 17,
-    name: "Memorial of The Blessed Virgin Mary, Mother of the Church",
-  },
-  {
-    month: 5,
-    day: 20,
-    name: "The Feast of Lord Jesus Christ, the Eternal High Priest",
-  },
-  {
-    month: 5,
-    day: 23,
-    name: "Solemnity of the Most Holy Trinity",
-  },
-  {
-    month: 5,
-    day: 30,
-    name: "Solemnity of the Most Holy Body and Blood of Christ",
-  },
-  {
-    month: 5,
     day: 31,
     name: "Feast of the Visitation of the Blessed Virgin Mary",
-  },
-  /* JUNE */
-  {
-    month: 6,
-    day: 4,
-    name: "Solemnity of the Most Sacred Heart of Jesus",
-  },
-  {
-    month: 6,
-    day: 5,
-    name: "Memorial of the Immaculate Heart of the Blessed Virgin Mary",
   },
   {
     month: 6,
@@ -161,18 +70,17 @@ export const feastDays: FeastDay[] = [
     day: 29,
     name: "Solemnity of Sts. Peter and Paul, Apostles",
   },
-  /* JULY */
   {
     month: 7,
     day: 16,
     name: "Memorial of Our Lady of Mount Carmel",
   },
-  /* AUGUST */
   {
     month: 8,
     day: 6,
     name: "Feast of the Transfiguration of the Lord",
   },
+
   {
     month: 8,
     day: 16,
@@ -188,7 +96,6 @@ export const feastDays: FeastDay[] = [
     day: 29,
     name: "Memorial of the Passion of St. John the Baptist",
   },
-  /* SEPTEMBER */
   {
     month: 9,
     day: 8,
@@ -209,7 +116,6 @@ export const feastDays: FeastDay[] = [
     day: 24,
     name: "Memorial of Our Lady of Walsingham",
   },
-  /* OCTOBER */
   {
     month: 10,
     day: 2,
@@ -220,7 +126,6 @@ export const feastDays: FeastDay[] = [
     day: 7,
     name: "Memorial of Our Lady of the Rosary",
   },
-  /* NOVEMBER */
   {
     month: 11,
     day: 1,
@@ -239,19 +144,13 @@ export const feastDays: FeastDay[] = [
   {
     month: 11,
     day: 18,
-    name: " The Dedication of the Basilicas of Saint Peter and Saint Paul in Rome",
+    name: "The Dedication of the Basilicas of Saint Peter and Saint Paul in Rome",
   },
   {
     month: 11,
     day: 21,
     name: "The Presentation of the Blessed Virgin Mary",
   },
-  {
-    month: 11,
-    day: 22,
-    name: "Our Lord Jesus Christ, King of the Universe",
-  },
-  /* DECEMBER */
   {
     month: 12,
     day: 8,
@@ -260,21 +159,130 @@ export const feastDays: FeastDay[] = [
   {
     month: 12,
     day: 10,
-    name: "Memoerial of Our Lady of Loreto",
+    name: "Memorial of Our Lady of Loreto",
   },
   {
     month: 12,
     day: 12,
-    name: "Memoerial of Our Lady of Guadalupe",
+    name: "Memorial of Our Lady of Guadalupe",
   },
   {
     month: 12,
     day: 25,
     name: "Solemnity of the Nativity of Our Lord or Christmas",
   },
+];
+
+const movingFeastDays: FeastDay[] = [
+  {
+    month: 1,
+    day: 11,
+    name: "Feast of the Baptism of the Lord",
+  },
+  {
+    month: 2,
+    day: 17,
+    name: "Shrove Tuesday",
+  },
+  {
+    month: 2,
+    day: 18,
+    name: "Ash Wednesday",
+  },
+  {
+    month: 3,
+    day: 29,
+    name: "Palm Sunday of the Passion of the Lord",
+  },
+  {
+    month: 4,
+    day: 2,
+    name: "Holy Thursday of the Lord's Supper",
+  },
+  {
+    month: 4,
+    day: 3,
+    name: "Good Friday",
+  },
+  {
+    month: 4,
+    day: 4,
+    name: "Holy Saturday",
+  },
+  {
+    month: 4,
+    day: 5,
+    name: "Easter Sunday",
+  },
+  {
+    month: 4,
+    day: 12,
+    name: "Feast of Divine Mercy",
+  },
+  {
+    month: 5,
+    day: 17,
+    name: "Solemnity of the Ascension of the Lord",
+  },
+  {
+    month: 5,
+    day: 24,
+    name: "Pentecost",
+  },
+  {
+    month: 5,
+    day: 25,
+    name: "Memorial of The Blessed Virgin Mary, Mother of the Church",
+  },
+  {
+    month: 5,
+    day: 28,
+    name: "The Feast of Lord Jesus Christ, the Eternal High Priest",
+  },
+  {
+    month: 5,
+    day: 31,
+    name: "Solemnity of the Most Holy Trinity",
+  },
+  {
+    month: 6,
+    day: 4,
+    name: "Solemnity of the Most Holy Body and Blood of Christ",
+  },
+  {
+    month: 6,
+    day: 12,
+    name: "Solemnity of the Most Sacred Heart of Jesus",
+  },
+  {
+    month: 6,
+    day: 13,
+    name: "Memorial of the Immaculate Heart of the Blessed Virgin Mary",
+  },
+
+  {
+    month: 11,
+    day: 22,
+    name: "Our Lord Jesus Christ, King of the Universe",
+  },
   {
     month: 12,
     day: 27,
     name: "Feast of the Holy Family of Jesus, Mary and Joseph",
   },
+];
+
+export const feastDays: FeastDay[] = [
+  /* JANUARY */
+  /* FEBRUARY */
+  /* MARCH */
+  /* APRIL */
+  /* MAY */
+  /* JUNE */
+  /* JULY */
+  /* AUGUST */
+  /* SEPTEMBER */
+  /* OCTOBER */
+  /* NOVEMBER */
+  /* DECEMBER */
 ];
